8651e777fbfce8c6e213094523e66180477e0e4f
jelenal23 <loncarj23@gmail.com>
2024-03-07 15:54:12 +0100

    main array sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,28 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "rootroot");
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+        //sort array
+
+        int array[] = new int[]{9,8,5,1,0,-5};
+        for(int i=0; i<array.length; i++) {
+            for(int j=i+1; j<array.length; j++){
+                int temp=0;
+            //rastuci niz
+                if(array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+            System.out.println(array[i]);
         }
 
     }
